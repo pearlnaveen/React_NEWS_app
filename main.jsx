fa8d09a9ce66000f0001fdfaa97f1d9c10b5bd91
Muthu Naveen N <pearlnaveen06@gmail.com>
2026-09-02 17:11:13 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
+
+//to create a react news app that diaplays news articles. expected outcome feed news online only change in app.jsx a simple app only
